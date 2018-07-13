30f8f21abc8ddd9314b98ffdec223befcf30b792
isabellademetz <isabella21136@gmail.com>
2018-07-13 16:26:34 +0000

    automatically backed up by learn

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -2,42 +2,19 @@
 class GardenGnome
 attr_accessible :name, :age, :gluten_allergy, :hat_color 
    attr_reader :personality, :gnaw, :shout, :introduce_self
-  def name
-    @name
-  end
-  def name=(new_name)
-    @name = new_name
-  end
-  def age
-    @age
-  end
-  def age=(new_age)
-    @age = new_age
-  end
-  def gluten_allergy
-    @gluten_allergy
-  end
-  def gluten_alergy=(new_gluten_alergy)
-    @gluten_alergy = new_gluten_alergy
-  end
-  def personality
-    @personality
-  end
-  GardenGnome.new(red)
-  def hat_color
-    @hat_color
-  end
-  def hat_color=(new_hat_color)
-    @hat_color=new_hat_color
-  end
+    def initialize(personality = "evil", hat_color = "red")
+   @personality = "evil"
+   @hat_color = hat_color
+
+
   def gnaw
-    gets "Gnawing on a tree!!"
+    returns "Gnawing on a tree!!"
   end
   def shout
-    gets "GNARLY!!!"
+    returns "GNARLY!!!"
   end
   def introduce_self
-    gets "Hello humans, my name is #{name}, I am #{age} years old, and youll rue the day you crossed me!"
+    returns "Hello humans, my name is #{name}, I am #{age} years old, and youll rue the day you crossed me!"
   end
 end
  
